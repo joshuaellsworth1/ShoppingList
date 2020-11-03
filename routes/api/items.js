17bfc5f5d7acfe443d7e9220c65746aa0fa42f1b
Josh Ellsworth <joshuaellsworth1@gmail.com>
2020-11-02 22:44:35 -0500

    added get, post, and now delete for Postman requests that are sent to mLab database

--- routes/api/items.js
+++ routes/api/items.js
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 });
 
 // @route       GET api/items
-// @description Create a Post
+// @description Create an item
 // @access      Public
 router.post('/', (req, res) => {
     const newItem = new Item({
@@ -23,6 +23,16 @@ router.post('/', (req, res) => {
 
     newItem.save()
     .then(item => res.json(item));
+});
+
+// @route       DELETE api/items/:id
+// @description Create an item
+// @access      Public
+router.delete('/:id', (req, res) => {
+ Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({ success: false }));
+
 });
 
 module.exports = router;
